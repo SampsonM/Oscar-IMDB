52a6d8d189e1ee31f57feae7c9c06049e9161686
SampsonM <SampsonM@github.com>
2018-05-15 12:33:46 +0100

    rm config

--- config/development.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    DB_URL: 'mongodb://localhost:27017/movie_night_dev',
-    PORT: 9090
-}
--- config/test.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  DB_URL: 'mongodb://localhost:27017/movie_night_test',
-  PORT: 3000
-}
